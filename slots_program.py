41eae7c9ee2bdb92775d2fb84b29dccb69c1dd99
Tawanda-Kamhuka99 <tawandak223@gmail.com>
2022-10-22 16:28:03 +0200

    feat: created function to get number of betting lines from user

--- slots_program.py
+++ slots_program.py
@@ -1,18 +1,42 @@
+#Set constants 
+MAX_LINES = 3
+
 #Deposit function which collects user deposit amount
 def deposit():
     while True:
-        amount = input("Please enter a deposit amount: $")
+        amount = input("\nPlease enter a deposit amount: $")
         if amount.isdigit():
             amount = int(amount)
             if amount > 10:
-                print(f"You deposited ${amount}.")
+                print(f"\nYou deposited ${amount}.")
                 break
             else:
-                print("Please enter an amount greater than $10.")
+                print("\nPlease enter an amount greater than $10.")
         else:
-            print("Please enter a number.")
+            print("\nPlease enter a number.")
     
     return amount
 
-deposit()
+#Function which gets number of lines user wants to bet on
+def get_lines():
+    while True:
+        lines = input("\nSelect the number of lines to bet on (1-" + str(MAX_LINES) + ") : ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                print(f"\nYou have chosen {lines} lines.")
+                break
+            else:
+                print("\nPlease enter a valid amount of lines.")
+        else:
+            print("\nPlease enter number of lines.")
+    
+    return lines
 
+
+#Create main function 
+def main():
+    balance = deposit()
+    lines = get_lines()
+
+main()
